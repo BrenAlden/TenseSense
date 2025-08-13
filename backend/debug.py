f1ce8dedf808ad9dbcff5f0919d6b04f3e0457e8
Bren Alden <164768427+BrenAlden@users.noreply.github.com>
2025-08-13 12:47:49 +0700

    Delete backend directory

--- backend/debug.py
+++
@@ -1,7 +0,0 @@
-import os
-
-print("--- Memeriksa Lokasi dan File ---")
-print(f"Direktori saat ini: {os.getcwd()}")
-print("\nFile dan folder di direktori ini:")
-print(os.listdir('.'))
-print("\nSelesai.")
